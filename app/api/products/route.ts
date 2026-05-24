793cbc0d846c446da59f9c72805a0900170a7d78
Lingeshwar G <lingeshwar1705@gmail.com>
2026-05-24 13:22:23 +0530

    Add products API endpoint

---
+++ app/api/products/route.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const products = await prisma.product.findMany({
+      include: {
+        inventories: {
+          include: {
+            warehouse: true
+          }
+        }
+      }
+    });
+
+    return NextResponse.json(products);
+  } catch (error) {
+    return NextResponse.json({
+      error: "Failed to fetch products"
+    });
+  }
+}
